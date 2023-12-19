375fc0cc0995842c1859327cec427d7d2fc11cb5
LDH <dlekgus0205@naver.com>
2023-12-19 09:19:28 +0900

    231219_DH_01

--- apill_web/src/components/N01mainpage/FlyoutMenus.jsx
+++ apill_web/src/components/N01mainpage/FlyoutMenus.jsx
@@ -41,7 +41,7 @@ export default function Menu() {
         leaveFrom="opacity-100 translate-y-0"
         leaveTo="opacity-0 translate-y-1"
       >
-        <Popover.Panel className="absolute right-[-10px] z-10 mt-5 flex w-screen max-w-max px-4">
+        <Popover.Panel className="absolute right-[-20px] z-10 mt-5 flex w-screen max-w-max px-4">
           <div className="w-screen flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5"
           style={{maxWidth:'23rem'}}>
             <div className="p-4">
