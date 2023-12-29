cd29edda3b9a8f942280a65ae0f16740ba441e4d
ohseungheon <ohseungheon3443@gamil.com>
2023-12-29 12:53:02 +0900

    DB 데이터 수정 후 커밋

--- apill_app/lib/screen/homepage/getSelectDate.dart
+++ apill_app/lib/screen/homepage/getSelectDate.dart
@@ -9,10 +9,17 @@ Future<List<DateTime>> getSelectDates(selectedDate) async {
   // 선택한 날짜의 오후 6시
   DateTime endDate = DateTime(selectedDate.year, selectedDate.month, selectedDate.day, 18, 0, 0);
 
+  // String sql = '''
+  //   SELECT *
+  //   FROM mibandtotal
+  //   WHERE start_time >= :start AND start_time < :end
+  // ''';
+
   String sql = '''
-    SELECT *
-    FROM mibandtotal
-    WHERE start_time >= :start AND start_time < :end
+  SELECT mtotal.start_time as instart mtotal.end_time as inend, intotal, minterval.*
+  FROM mibandtotal mtotal
+  JOIN mibandinterval minterval ON minterval.start_time >= mtotal.start_time AND minterval.end_time <= mtotal.end_time
+  WHERE mtotal.start_time >= :start AND mtotal.end_time <= :end
   ''';
 
   var result = await dbConnector(sql, {
@@ -22,9 +29,11 @@ Future<List<DateTime>> getSelectDates(selectedDate) async {
 
   if (result != null) {
     for (final row in result) {
-      // 여기에서 dateList에 값을 추가하거나 원하는 처리를 수행하세요.
-      // 예시: dateList.add(row['start_time']);
-      print(row.assoc());
+      print((row.assoc()));
+      // print("수면시작시간 : ${row.assoc()['start_time']},"
+      //     " 수면종료시간 : ${row.assoc()['end_time']}");
+
+
     }
   }
 
