457e3b0ae5aed855486e75333cf893059751d535
ohseungheon <ohseungheon3443@gamil.com>
2024-01-12 17:28:06 +0900

    초기화 변경

--- apill_app/lib/screen/main_page/bottom_navi_page.dart
+++ apill_app/lib/screen/main_page/bottom_navi_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
